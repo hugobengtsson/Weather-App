29e65e70abb1db41ff03a0adfcf2ca0a2994f933
Hugo Bengtsson <hugo@capellium.se>
2022-08-17 18:15:24 +0200

    Initial layout on home screen & types for api

--- functions/main.ts
+++ functions/main.ts
@@ -1,12 +1,21 @@
 import { IP } from "../ip";
 
-export default async function makeRequest() {
+export default async function makeRequest(city: string) {
 
-    let response = await fetch(`http://${IP}/api/city/laholm`);
+    let response = await fetch(`http://${IP}/api/city/${city}`);
 
-    let result = await response.json();
-
+    let result: CityObject[] = await response.json();
     console.log(result)
+    return result;
 
 
+}
+
+
+
+export interface CityObject {
+    name: string,
+    region: string,
+    long: string,
+    lat: string,
 }
